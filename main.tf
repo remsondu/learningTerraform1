fef724319ee89eeec768b528b3457429d403c017
Bowale Oyenuga <48326695+remsondu@users.noreply.github.com>
2022-02-05 10:31:33 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,107 @@
+variable "whitelist" {
+  type = list(string)
+}
+variabe "web_image_id" {
+  type = string
+}
+variable "web_instance_type" {
+  type = string
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "prod_tf_course" {
+  bucket = "tf-course-remsondui"
+  acl    = "private"
+}
+
+resource "aws_default_vpc" "defaulf" {}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-west-2b"
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbound"
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.whitelist
+  } 
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.whitelist
+  }
+  egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_blocks = var.whitelist
+  }
+ 
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_instance" "prod_web" {
+  count = 2
+
+  ami           = var.web_image_id
+  instance_type = var.web_instance_type
+
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_eip_association" "prod_web" {
+  instance_id = aws_instance.prod_web.0.id
+  allocation_id = aws_eip.prod_web.id
+}
+
+resource "aws_eip" "prod_web" {
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+}
+tags = {
+  "Terraform" : "true"
+  }
+}
+
---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+whitelist         = ["0.0.0.0/0"]
+web_image_id      = "ami-03a0cd500c36e8f3c"
+web_instance_type = "t2.nano"
+
+
